e028e5b27badf4abfcf1af82660b6a286167ea47
Umair <umair.mehmood15@gmail.com>
2026-01-30 19:41:06 +0000

    Format data and add catch block for error handling

--- week1-personal-dashboard/backend/server.js
+++ week1-personal-dashboard/backend/server.js
@@ -18,8 +18,43 @@ app.get('/api/weather', async (req, res) => {
 // req - Request: Object containing details about the incoming request.
 // res - Response: Object sued to send a response back to the frontend.
 try {
-    const city = req.query.city || 'London';
-    const apiKey = process.env.OPENWEATER_API_KEY;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const city = req.query.city || 'London'; // Request data for the specified city.
+    const apiKey = process.env.OPENWEATER_API_KEY; // Retrieve API key from .env file.
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; // Call OpenWeatherMap API by plugging in city and the API key retrieved above.
+
+    const response = await fetch(apiUrl); // Pauses the function until the request to the API (above) is complete (asynchronous)
+    if (!response.ok) {
+        throw new Error(`OpenWeatherMap API error: ${response.status}`)
+    } // Checks if the response status is not in 200d (404, 401, etc.). If so, throws an error to jump to the catch block.
+    const weatherData = await response.json(); // Parses the JSON text response from OpenWeatherMap into a usable JS object (weatherData)
+
+    res.json({
+        // Send the final formatted data back to the frontend as JSON.
+
+        success:true,
+        city: weatherData.name,
+        temparature: Math.round(weatherData.main.temp),
+        feels_like: Math.round(weatherData.main.feels_like),
+        description: weatherData.weather[0].description,
+        humidity: weatherData.main.humidity,
+        icon: weatherData.weather[0].icon
+    });
+} catch (error) {
+    // Send a user-friendly error to the frontend
+    res.status(500).json({
+        success: false,
+        error: 'Could not fetch weather data. Please check the city name or try again later.'
+    });
 }
+
+// try... catch - This block wraps the process so that if anything fails, execution jumps to the catch.
+// console.error() - Logs the detailed technical error to the terminal to make debugging easier.
+// res.status(500).json() - Sends a simple user-friendly error message back to the frontend. if fetchWeather function is 'false' it will display this message.
 });
+
+app.listen(PORT, () => {
+    console.log(`✅ Proxy server running: http://localhost:${PORT}`)
+})
+// app.listen(PORT, ...) - Command to start the server. Tells Express to begin listening for incoming requests on the specified PORT.
+// Log success message to terminal once the server is ready.
