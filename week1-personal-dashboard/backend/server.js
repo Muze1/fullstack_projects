f64dc7e4392849498cbc3ced4f650e05db7c08fe
Umair <umair.mehmood15@gmail.com>
2026-01-30 18:42:20 +0000

    Set up imports for server

---
+++ week1-personal-dashboard/backend/server.js
@@ -0,0 +1,4 @@
+const express = require('express'); // Imports Express.js library
+const fetch = require('node-fetch'); // Imports a library that allows your Node.js server to make HTTP requests to other servers (like OpenWeatherMap's API for example)
+require('dotenv').config(); // Loads the environment variables from the .env file into the process.env object
+
