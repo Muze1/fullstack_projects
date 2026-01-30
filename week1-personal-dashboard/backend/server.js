4aafc2aa500507cba3dcf552652f780d6f4ac64c
Umair <umair.mehmood15@gmail.com>
2026-01-30 18:53:20 +0000

    Add CORS to allow requests from any origin

--- week1-personal-dashboard/backend/server.js
+++ week1-personal-dashboard/backend/server.js
@@ -5,3 +5,9 @@ require('dotenv').config(); // Loads the environment variables from the .env fil
 const app = express(); // Creates an instance of an Express application which will be the server
 const PORT = process.env.PORT || 3000; // Set the port number for the server. Defaults to 3000 if port not found in .env
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+}) // By default, browsers block web pages from making requests to different origin for security. CORS middleware allows frontend to talk to backend: Access-Control-Allow-Origin tells browser it's okay to accept requests from any origin (*).
+
