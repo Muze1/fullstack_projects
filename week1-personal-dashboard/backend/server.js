90d7e5ce456f035206fdab3ac4683a06589e7baf
Umair <umair.mehmood15@gmail.com>
2026-01-30 18:45:51 +0000

    Create server and define port

--- week1-personal-dashboard/backend/server.js
+++ week1-personal-dashboard/backend/server.js
@@ -1,4 +1,7 @@
 const express = require('express'); // Imports Express.js library
-const fetch = require('node-fetch'); // Imports a library that allows your Node.js server to make HTTP requests to other servers (like OpenWeatherMap's API for example)
+const fetch = require('node-fetch'); // Imports a library that allows the Node.js server to make HTTP requests to other servers (like OpenWeatherMap's API for example)
 require('dotenv').config(); // Loads the environment variables from the .env file into the process.env object
+
+const app = express(); // Creates an instance of an Express application which will be the server
+const PORT = process.env.PORT || 3000; // Set the port number for the server. Defaults to 3000 if port not found in .env
 
