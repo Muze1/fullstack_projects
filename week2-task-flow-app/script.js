1e2831e5005d5d4f0b90bcb9a7a1056c286fb914
Umair <umair.mehmood15@gmail.com>
2026-02-07 15:41:30 +0000

    Add variables to store DOM elements

--- week2-task-flow-app/script.js
+++ week2-task-flow-app/script.js
@@ -4,3 +4,17 @@ const state = {
     nextId: JSON.parse(localStorage.getItem('taskFlowNextId')) || 1
 };
 
+// Retrieve & store DOM Elements
+const taskForm = document.getElementById('task-form');
+const taskInput = document.getElementById('task-input');
+const columns = {
+    todo: document.getElementById('todo-tasks'),
+    progress: document.getElementById('progress-tasks'),
+    done: document.getElementById('done-tasks')
+};
+const countElements = {
+    todo: document.getElementById('todo-count'),
+    progress: document.getElementById('progress-tasks'),
+    done: document.getElementById('done-count')
+};
+
